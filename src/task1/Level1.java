3400a6ed5a41209c57d2696dfe8c5973860eb9d4
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-01-29 21:50:42 +0300

    Lesson1

--- src/task1/Level1.java
+++ src/task1/Level1.java
@@ -1,8 +1,10 @@
 public class Level1 {
     public static int squirrel(int number){
         int factorial = 1;
-        if (number <= 0)
+        if (number < 0)
            return -1;
+        if (number == 0)
+            return 1;
         for (int i = 1; i <= number ; i++) {
             factorial *= i;
         }
