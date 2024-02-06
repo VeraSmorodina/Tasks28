fe6e61e4e9d5ad065db9b6df7ec1df9ae0c07e62
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-02-06 19:00:01 +0300

    Lesson5

--- src/task5/Level1.java
+++ src/task5/Level1.java
@@ -1,8 +1,4 @@
-package task5;
-
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Level1 {
     public static int[] SynchronizingTables(int N, int[] ids, int[] salary) {
@@ -10,13 +6,8 @@ public class Level1 {
         Arrays.sort(salary1);
         int[] ids1 = ids.clone();
         Arrays.sort(ids1);
-
-        Map<Integer, Integer> idToIndex = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            idToIndex.put(ids[i], i);
-        }
-        for (int i = 0; i < ids1.length; i++) {
-            for (int j = 0; j < ids.length; j++) {
+            for (int j = 0; j < N; j++) {
                 if (ids1[i] == ids[j])
                     salary[j] = salary1[i];
             }
