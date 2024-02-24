6970c3c0a7d52507330ee2df267cdd0afeff3bca
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-02-24 13:57:46 +0300

    Lesson11

--- src/task11/Level1.java
+++ src/task11/Level1.java
@@ -1,18 +1,21 @@
+package task11;
+
 import java.util.ArrayList;
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.List;
 
 public class Level1 {
     public static String BigMinus(String s1, String s2) {
         int n = Math.max(s1.length(), s2.length());
-        int[] num1;
-        int[] num2;
+        Integer[] num1;
+        Integer[] num2;
         if (s1.length() > s2.length()) {
             num1 = x(s1, n);
             num2 = x(s2, n);
         } else if (s1.length() == s2.length()) {
-            ArrayList<Integer[]> arrayList = y(s1, s2, n);
-            num1 = z(arrayList.get(0));
-            num2 = z(arrayList.get(1));
+            List<Integer[]> arrayList = y(s1, s2, n);
+            num1 = arrayList.get(0);
+            num2 = arrayList.get(1);
         } else {
             num1 = x(s2, n);
             num2 = x(s1, n);
@@ -35,18 +38,19 @@ public class Level1 {
         return result;
     }
 
-    public static int[] x(String s, int N) {
-        int[] num = new int[N];
+    public static Integer[] x(String s, int N) {
+        Integer[] num = new Integer[N];
+        Arrays.fill(num, 0);
         for (int i = 0; i < s.length(); i++) {
             num[N - s.length() + i] = s.charAt(i) - '0';
         }
         return num;
     }
 
-    public static ArrayList<Integer[]> y(String s1, String s2, int N) {
+    public static List<Integer[]> y(String s1, String s2, int N) {
         ArrayList<Integer[]> arrayList = new ArrayList<>();
-        Integer[] array1 = IntStream.of(x(s1, N)).boxed().toArray(Integer[]::new);
-        Integer[] array2 = IntStream.of(x(s2, N)).boxed().toArray(Integer[]::new);
+        Integer[] array1 = x(s1, N);
+        Integer[] array2 = x(s2, N);
         boolean bS1 = false;
         for (int i = 0; i < N; i++) {
             if (array1[i] > array2[i]) {
@@ -54,21 +58,8 @@ public class Level1 {
                 break;
             }
         }
-        if (bS1) {
-            arrayList.add(array1);
-            arrayList.add(array2);
-        } else {
-            arrayList.add(array2);
-            arrayList.add(array1);
-        }
+        arrayList.add(bS1 ? array1 : array2);
+        arrayList.add(bS1 ? array2 : array1);
         return arrayList;
-    }
-
-    public static int[] z(Integer[] ar) {
-        int[] result = new int[ar.length];
-        for (int i = 0; i < ar.length; i++) {
-            result[i] = ar[i];
-        }
-        return result;
     }
 }
