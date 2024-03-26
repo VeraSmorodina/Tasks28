d77ce3dd55257da32509ef783d37aaa5cfb07519
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-03-26 16:28:53 +0300

    Lesson21

--- src/task21/Level1.java
+++ src/task21/Level1.java
@@ -1,37 +1,26 @@
-package task21;
-
 import java.util.*;
 
 public class Level1 {
-    public static String w = "";
+    public static String word = "";
 
     public static String BiggerGreater(String input) {
-        Set<String> uniquePermutations = new HashSet<>();
-        w = input;
-        generatePermutations(input.toCharArray(), 0, uniquePermutations);
-//        Set<String> uniquePermutations = BiggerGreater(input);
-        List<String> strings1 = new ArrayList<>();
-        strings1.addAll(new TreeSet<>(uniquePermutations));
-        for (String s : strings1) {
-            System.out.print(s + " + ");
-        }
-        System.out.println();
-        if (strings1.isEmpty())
-            return "";
-        else
-            return strings1.get(0);
+        word = input;
+        Set<String> uniquePermutations = generatePermutations(input.toCharArray(), 0, new HashSet<>());
+        List<String> strings1 = new ArrayList<>(new TreeSet<>(uniquePermutations));
+        return strings1.isEmpty() ? "" : strings1.get(0);
     }
 
-    private static void generatePermutations(char[] arr, int index, Set<String> uniquePermutations) {
-        if (index == arr.length && w.compareTo(new String(arr)) < 0) {
+    private static Set<String> generatePermutations(char[] arr, int index, Set<String> uniquePermutations) {
+        if (index == arr.length && word.compareTo(new String(arr)) < 0) {
             uniquePermutations.add(new String(arr));
-            return;
+            return uniquePermutations;
         }
         for (int i = index; i < arr.length; i++) {
             swap(arr, index, i);
             generatePermutations(arr, index + 1, uniquePermutations);
             swap(arr, index, i);
         }
+        return uniquePermutations;
     }
 
     private static void swap(char[] arr, int i, int j) {
@@ -39,33 +28,4 @@ public class Level1 {
         arr[i] = arr[j];
         arr[j] = temp;
     }
-
-//    public static void main(String[] args) {
-////        String str = "ая";
-////        String str = "нклм";
-////        String str = "fff";
-////        String str = "вибк";
-//        String str = "вкиб";
-//        w = str;
-//        Set<String> uniquePermutations = BiggerGreater(str);
-//        List<String> strings1 = new ArrayList<>();
-//        strings1.addAll(new TreeSet<>(uniquePermutations));
-//        for (String s : strings1) {
-//            System.out.print(s + " + ");
-//        }
-//        System.out.println();
-//        if (strings1.isEmpty())
-//            System.out.println("-");
-//        else
-//            System.out.println(strings1.get(0));
-////        for (String permutation : uniquePermutations) {
-////            if (permutation.compareTo(str) > 0){
-////                str = permutation;
-////                System.out.println(permutation);
-////            }
-////
-////            else System.out.println("-");
-//////            System.out.println(permutation);
-////        }
-//    }
 }
