3e75d508951fe209f8f21383250518645c7aadb7
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-03-26 16:14:32 +0300

    Lesson21

--- src/task21/Level1.java
+++ src/task21/Level1.java
@@ -5,11 +5,21 @@ import java.util.*;
 public class Level1 {
     public static String w = "";
 
-    public static Set<String> BiggerGreater(String str) {
+    public static String BiggerGreater(String input) {
         Set<String> uniquePermutations = new HashSet<>();
-        generatePermutations(str.toCharArray(), 0, uniquePermutations);
-//        return uniquePermutations;
-        return uniquePermutations;
+        w = input;
+        generatePermutations(input.toCharArray(), 0, uniquePermutations);
+//        Set<String> uniquePermutations = BiggerGreater(input);
+        List<String> strings1 = new ArrayList<>();
+        strings1.addAll(new TreeSet<>(uniquePermutations));
+        for (String s : strings1) {
+            System.out.print(s + " + ");
+        }
+        System.out.println();
+        if (strings1.isEmpty())
+            return "";
+        else
+            return strings1.get(0);
     }
 
     private static void generatePermutations(char[] arr, int index, Set<String> uniquePermutations) {
@@ -30,32 +40,32 @@ public class Level1 {
         arr[j] = temp;
     }
 
-    public static void main(String[] args) {
-//        String str = "ая";
-//        String str = "нклм";
-//        String str = "fff";
-//        String str = "вибк";
-        String str = "вкиб";
-        w = str;
-        Set<String> uniquePermutations = BiggerGreater(str);
-        List<String> strings1 = new ArrayList<>();
-        strings1.addAll(new TreeSet<>(uniquePermutations));
-        for (String s : strings1) {
-            System.out.print(s + " + ");
-        }
-        System.out.println();
-        if (strings1.isEmpty())
-            System.out.println("-");
-        else
-            System.out.println(strings1.get(0));
-//        for (String permutation : uniquePermutations) {
-//            if (permutation.compareTo(str) > 0){
-//                str = permutation;
-//                System.out.println(permutation);
-//            }
-//
-//            else System.out.println("-");
-////            System.out.println(permutation);
+//    public static void main(String[] args) {
+////        String str = "ая";
+////        String str = "нклм";
+////        String str = "fff";
+////        String str = "вибк";
+//        String str = "вкиб";
+//        w = str;
+//        Set<String> uniquePermutations = BiggerGreater(str);
+//        List<String> strings1 = new ArrayList<>();
+//        strings1.addAll(new TreeSet<>(uniquePermutations));
+//        for (String s : strings1) {
+//            System.out.print(s + " + ");
 //        }
-    }
+//        System.out.println();
+//        if (strings1.isEmpty())
+//            System.out.println("-");
+//        else
+//            System.out.println(strings1.get(0));
+////        for (String permutation : uniquePermutations) {
+////            if (permutation.compareTo(str) > 0){
+////                str = permutation;
+////                System.out.println(permutation);
+////            }
+////
+////            else System.out.println("-");
+//////            System.out.println(permutation);
+////        }
+//    }
 }
