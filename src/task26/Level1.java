16cf6bb15a768738dad931d9ceb6326fc1242ea5
VeraSmorodina <v3ra.smorodina@yandex.ru>
2024-04-10 16:17:48 +0300

    Lesson26

--- src/task26/Level1.java
+++ src/task26/Level1.java
@@ -1,51 +1,76 @@
 package task26;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Level1 {
     public static void main(String[] args) {
-        System.out.println(white_walkers("axxb6===4xaf5===eee5"));
+        System.out.println(white_walkers("5==ooooooo=5=5"));
     }
-
     public static boolean white_walkers(String village) {
         ArrayList<String> array = new ArrayList<>();
         int startIndex = 0;
         boolean b = false;
-
         for (int i = 0; i < village.length(); i++) {
-//            if (village.charAt(i))
-            if (!village.substring(i, i + 1).matches("\\d")) {
-                System.out.println("+");
+            if (!village.substring(i, i + 1).matches("\\d"))
                 continue;
-            }
             if (!b) {
-                System.out.println("-");
                 startIndex = i;
                 b = true;
                 continue;
             }
-            System.out.println("finish");
-            array.add(village.substring(startIndex, i + 1));
+            String s = village.substring(startIndex, i + 1);
+//            if (s.length()-2 % 3 == 0)
+            if (s.length()-2 >= 3)
+                array.add(s);
+            s = "";
+
+
+
+//            array.add(village.substring(startIndex, i + 1));
             startIndex = i;
+            i--;
             b = false;
         }
-        for (int i = 0; i < array.size(); i++) {
-            System.out.println(array.get(i));
+
+        System.out.println(array.isEmpty() + "  usEmp");
+        for (String s : array) {
+            System.out.println(s);
         }
+
 
 
         for (String string : array) {
-            boolean b1 = false;
+            boolean b1 = true;
             for (int i = 1; i < string.length() - 1; i++) {
-                if (string.length() - 2 % 3 != 0)
-                    break;
                 if (string.charAt(i) != '=')
-                    b1 = true;
+                    b1 = false;
             }
-            if (!b1)
+            if (b1)
                 return true;
         }
         return false;
+
+
+
+
+//        for (String string : array) {
+//            boolean b1 = true;
+//
+//            for (int i = 1; i < string.length() - 1; i++) {
+//                if ((string.length() - 2) % 3 != 0){
+////                    b1 = false;
+//                    break;
+//                }
+////                else if ((string.length() - 2) % 3 != 0 && array.get(array.size()) == string){
+////                    b1 = false;
+////                    break;
+////                }
+//                if (string.charAt(i) != '=')
+//                    b1 = false;
+//            }
+//            if (b1)
+//                return true;
+//        }
+//        return false;
     }
 }
---
+++ test/task26/Level1Test.java
@@ -0,0 +1,42 @@
+package task26;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Level1Test {
+    @Test
+    public void white_walkers1() {
+        boolean result = Level1.white_walkers("axxb6===4xaf5===eee5");
+        boolean expected = true;
+        assert result == expected;
+    }
+
+    @Test
+    public void white_walkers2() {
+        boolean result = Level1.white_walkers("5==ooooooo=5=5");
+        boolean expected = false;
+        assert result == expected;
+    }
+
+    @Test
+    public void white_walkers3() {
+        boolean result = Level1.white_walkers("abc=7==hdjs=3gg1=======5");
+        boolean expected = true;
+        assert result == expected;
+    }
+
+    @Test
+    public void white_walkers4() {
+        boolean result = Level1.white_walkers("aaS=8");
+        boolean expected = false;
+        assert result == expected;
+    }
+
+    @Test
+    public void white_walkers5() {
+        boolean result = Level1.white_walkers("9===1===9===1===9");
+        boolean expected = true;
+        assert result == expected;
+    }
+}
